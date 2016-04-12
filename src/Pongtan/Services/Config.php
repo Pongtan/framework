b5087dce03dc88f9e2dca5029e1c4053aa2326f6
orvice <orvice@gmail.com>
2016-04-13 00:44:09 +0800

    hello from mars

---
+++ src/Pongtan/Services/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pongtan\Services;
+
+
+class Config
+{
+    public static function get($key)
+    {
+        $key = getenv($key);
+        if ($key == 'true') {
+            return true;
+        }
+        if ($key == 'false') {
+            return false;
+        }
+        return $key;
+    }
+}
