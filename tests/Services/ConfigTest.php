bb424847508a5f52c1d4b4a64132503f6a535fe4
orvice <orvice@gmail.com>
2016-05-16 22:50:06 +0800

    update config tests

--- tests/Services/ConfigTest.php
+++ tests/Services/ConfigTest.php
@@ -6,10 +6,20 @@ class ConfigTest extends PHPUnit_Framework_TestCase
 {
     public function testConfig()
     {
-        $key = "test_key";
-        $value = "test_value";
-        Config::set($key,$value);
-        $this->assertEquals($value,Config::get($key));
+        $key = "foo";
+        $value = "bar";
+        Config::set($key, $value);
+        $this->assertEquals($value, Config::get($key));
+
+        $true = 'true';
+        $false = 'false';
+
+        Config::set($key, $true);
+        $this->assertEquals(true, Config::get($key));
+
+        Config::set($key, $false);
+        $this->assertEquals(false, Config::get($key));
+
     }
-    
+
 }
