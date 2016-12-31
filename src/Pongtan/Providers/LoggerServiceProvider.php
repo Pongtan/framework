45c91d73772528fc8c72bd122accfc606009a01e
orvice <orvice@gmail.com>
2016-12-31 21:14:25 +0800

    fix log

--- src/Pongtan/Providers/LoggerServiceProvider.php
+++ src/Pongtan/Providers/LoggerServiceProvider.php
@@ -3,14 +3,16 @@
 namespace Pongtan\Providers;
 
 use Pongtan\Contracts\ServiceProviderInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class LoggerServiceProvider implements ServiceProviderInterface
 {
     public function register()
     {
         app()->singleton('log', function () {
-            $log = new MonoLogger(config('app.name'));
-            $log->pushHandler(new StreamHandler(app()->getBasePath().'/storage/logs/fish.log', config('app.log_level')));
+            $log = new Logger(config('app.name'));
+            $log->pushHandler(new StreamHandler(app()->getBasePath() . '/storage/logs/fish.log', config('app.log_level')));
 
             return $log;
         });
